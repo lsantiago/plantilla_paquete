fc801d03f7d7b7d491e8dd38f7fa897eb6b600d1
Santiago Quiñones-Cuenca <lsquinones@gmail.com>
2024-07-16 11:15:15 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nombre_paquete',
+    version='0.1.0',
+    description='.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/nombre_paquete',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy==1.24.1',
+    ],
+)
+
+
